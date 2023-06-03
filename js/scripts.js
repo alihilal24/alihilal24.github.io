f0168e03249ef05b2161db4916ce518f00169f6d
Ali Hilal <alihilal24@gmail.com>
2023-06-02 22:28:29 -0500

    added colors, fonts, and fixed navbar

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@
 // Scripts
 // 
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("mainNav").style.top = "0";
+  } else {
+    document.getElementById("mainNav").style.top = "-110px";
+  }
+  prevScrollpos = currentScrollPos;
+}
