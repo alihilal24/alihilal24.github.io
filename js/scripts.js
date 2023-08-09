9d3e0af7e881e8533c3e0b397b1ba36d5c0914fc
Ali Hilal <alihilal24@gmail.com>
2023-08-08 20:28:47 -0500

    finished formatting project section

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,30 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+function openCity(evt, companyName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  
+  
+  // Get all elements with class="experience-tab-content" and hide them
+  tabcontent = document.getElementsByClassName("experience-tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  
+  // Get all elements with class="experience-tab-button" and remove the class "active"
+  tablinks = document.getElementsByClassName("experience-tab-button");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  if (companyName == "") {
+    document.getElementById("linedanceAI").style.display = "block";
+  evt.currentTarget.className += " active";
+  }
+  
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(companyName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
