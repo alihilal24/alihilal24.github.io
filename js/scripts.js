d4cf9fd8b013cda1fac0beae28d5bd828d40815a
Ali Hilal <alihilal24@gmail.com>
2023-08-21 23:04:10 -0500

    contect me section completed

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,22 @@ function openCity(evt, companyName) {
   document.getElementById(companyName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+const txHeight = 16;
+const tx = document.getElementsByTagName("textarea");
+
+for (let i = 0; i < tx.length; i++) {
+  if (tx[i].value == '') {
+    tx[i].setAttribute("style", "height:" + txHeight + "px;overflow-y:hidden;");
+  } else {
+    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;padding:10px 10px 0px 10px");
+  }
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput(e) {
+  this.style.height = 0;
+  this.style.height = (this.scrollHeight) + "px";
+  
+}
+
